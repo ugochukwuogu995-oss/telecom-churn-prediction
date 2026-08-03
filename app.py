33d56df93a0eab575bdf91f7bcb6b24bf1506fb3
ugochukwuogu995-oss <ugochukwuogu995@gmail.com>
2026-08-03 19:35:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 from pathlib import Path
 
 MODEL_PATH = Path(__file__).parent / "churn_model.pkl"
-model = joblib.load(MODEL_PATH)
+model = pathlib.load(MODEL_PATH)
 st.set_page_config(
     page_title="Telecom Customer Churn Prediction",
     page_icon="📱",
